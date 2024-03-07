a7913677db5c16e1a88f0f7295834355c9d1cad8
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2024-03-07 21:34:59 +0800

    Added types and regretting it

--- resolver.erl
+++ resolver.erl
@@ -1,22 +1,26 @@
+%% resolver - DNS resolver.
 -module(resolver).
 
 -export([send_query/3, build_query/2, number_to_record_type/1]).
 
--record(dns_header, {id,
-                     flags = 0,
-                     n_questions = 0,
-                     n_answers = 0,
-                     n_authorities = 0,
-                     n_additionals = 0}).
+-type u16() :: 0..65535.
+-type record_type() :: a | cname.
+-record(dns_header, {id :: u16(),
+                     flags = 0 :: u16(),
+                     n_questions = 0 :: u16(),
+                     n_answers = 0 :: u16(),
+                     n_authorities = 0 :: u16(),
+                     n_additionals = 0 :: u16()}).
 
 send_query(IPAddress, DomainName, RecordType) ->
     Query = build_query(DomainName, RecordType),
     {ok, Socket} = gen_udp:open(0, [inet, binary, {active, false}]),
-    gen_udp:send(Socket, IPAddress, 53, Query),
+    ok = gen_udp:send(Socket, IPAddress, 53, Query),
     Reply = catch gen_udp:recv(Socket, 1024, 30 * 1000),
     gen_udp:close(Socket),
     Reply.
 
+-spec build_query(string(), record_type()) -> iolist().
 build_query(DomainName, RecordType) ->
     ID = random_id(),
     Header = header_to_bytes(#dns_header{id = ID,
@@ -24,9 +28,11 @@ build_query(DomainName, RecordType) ->
     Question = question_to_bytes(DomainName, RecordType, in),
     [Header, Question].
 
+-spec random_id() -> u16().
 random_id() ->
     rand:uniform(65536) - 1.
 
+-spec header_to_bytes(#dns_header{}) -> <<_:96>>.
 header_to_bytes(#dns_header{id = ID,
                             flags = Flags,
                             n_questions = NQuestions,
@@ -49,8 +55,9 @@ question_to_bytes({encoded, EncodedName}, RecordType, Class) ->
       <<RecordTypeInt:16/big,
         ClassInt:16/big>>].
 
+-spec encode_dns_name(string()) -> iolist().
 encode_dns_name(Name) ->
-    [[<<Length:8, Component/binary>> || {Length, Component} <- to_components(Name)], 0].
+    [[[Length, Component] || {Length, Component} <- to_components(Name)], 0].
 
 to_components(Name) ->
     lists:reverse(to_components(Name, [], [])).
@@ -65,9 +72,11 @@ to_components([$.|Rest], Current, Acc) ->
 to_components([Char|Rest], Current, Acc) ->
     to_components(Rest, [Char|Current], Acc).
 
+-spec as_component(string()) -> {non_neg_integer(), string()}.
 as_component(ReversedComponent) ->
-    {length(ReversedComponent), list_to_binary(lists:reverse(ReversedComponent))}.
+    {length(ReversedComponent), lists:reverse(ReversedComponent)}.
 
+-spec record_type_to_number(record_type()) -> 0..65535.
 record_type_to_number(a) -> 1;
 record_type_to_number(ns) -> 2;
 record_type_to_number(cname) -> 5.
