c58a2351c40306c9f571a02a226d7949d2b9212b
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2024-03-12 09:51:34 +0800

    wip: implement more cache

--- test/test_dns_cache.erl
+++ test/test_dns_cache.erl
@@ -1,7 +1,7 @@
 -module(test_dns_cache).
 
 -include_lib("eunit/include/eunit.hrl").
--include("src/dns.hrl").
+-include("include/dns.hrl").
 
 
 check_expired_at_creation_time_test() ->
@@ -25,10 +25,55 @@ check_expired_true_realistic_test() ->
   Entry = {-576460735, dns_record_with_ttl(4)},
   true = dns_cache:expired(Entry, -576460706).
 
+cache_fetch_from_empty_test() ->
+  Empty = dns_cache:new(),
+  Name = "not.important",
+  {Empty, miss} = dns_cache:get_all_records(Empty, Name, 0).
+
+cache_simple_record_test() ->
+  Empty = dns_cache:new(),
+  InsertTime = 0,
+  Record = dns_record_with_ttl(half_hour()),
+  Name = name(Record),
+  C2 = dns_cache:add_records(Empty, [Record], InsertTime),
+
+  % About a minute later...
+  FetchTime = rand:uniform(60),
+  {_, {hit, [Record]}} = dns_cache:get_all_records(C2, Name, FetchTime).
+
+cache_fetch_expired_record_test() ->
+  Empty = dns_cache:new(),
+  InsertTime = 0,
+  Record = dns_record_with_ttl(half_hour()),
+  Name = name(Record),
+  C2 = dns_cache:add_records(Empty, [Record], InsertTime),
+
+  % Just after the expiration time...
+  FetchTime = half_hour() + 1,
+  {Empty, expired} = dns_cache:get_all_records(C2, Name, FetchTime).
+
+cache_inconsistent_names_test() ->
+  Empty = dns_cache:new(),
+  InsertTime = 0,
+  InsertName = "Localhost",
+  Record = dns_record_with_ttl(InsertName, half_hour()),
+  C2 = dns_cache:add_records(Empty, [Record], InsertTime),
+
+  % Fetch it right after:
+  FetchTime = 1,
+  FetchName = "LOCALHOST",
+  {C2, {hit, [Result]}} = dns_cache:get_all_records(C2, FetchName, FetchTime),
+  "localhost" = name(Result).
+
 
 % Utilties %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%% Create a fake DNS record with the given TTL.
+%% @doc Create a fake DNS record with the given TTL and optional name.
 dns_record_with_ttl(TTL) ->
-  #dns_record{name="localhost", type=a,
+  dns_record_with_ttl("localhost", TTL).
+dns_record_with_ttl(Name, TTL) ->
+  #dns_record{name=Name, type=a,
               class=in, ttl=TTL, data ={127, 0, 0, 1}}.
+
+name(#dns_record{name = Name}) -> Name.
+half_hour() -> 30 * 60.
